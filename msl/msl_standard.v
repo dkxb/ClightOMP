81b97e178f25833fd8078bf23d00edd5bc2d4525
Anshu Sharma <anshu.g.sharma@gmail.com>
2026-05-04 14:11:28 -0400

    adding coq-iris and coq-mathcomp-ssreflect to builddep and adding msl and shared repos

---
+++ msl/msl_standard.v
@@ -0,0 +1,7 @@
+Require Export VST.msl.Extensionality.
+Require Export VST.msl.base.
+Require Export VST.msl.sepalg.
+Require Export VST.msl.shares.
+Require Export VST.msl.psepalg.
+Require Export VST.msl.pshares.
+Require Export VST.msl.eq_dec.
